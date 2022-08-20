360541614f6cc76abeea67f445f3e1480b2f253a
supernovatux <thulashitharan.d@gmail.com>
2022-08-20 21:43:22 +0530

    Added system tray

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
-//use std::{env, fs};
-
 use auto_backlight::{
-    brightness::BrightnessDevices, cli_parser::{get_limit, get_refresh}, init, screens::change_calc,
+    brightness::BrightnessDevices,
+    cli_parser::{get_limit, get_refresh},
+    init,
+    screens::change_calc,
+    sys_tray,
 };
-use std::time::Duration;
-use std::thread::sleep;
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread::{self, sleep},
+    time::Duration,
+};
 
 fn main() {
     init();
+    let brightnessctl_status = Arc::new(AtomicBool::new(true));
+    let status_to_send = brightnessctl_status.clone();
     let mut brightness = 0;
     let mut change = 0;
     let brightness_dev = BrightnessDevices::new();
+    thread::spawn(move || sys_tray::start_knsi(status_to_send));
     loop {
-        let change_new = change_calc(get_limit() as u8);
-        if change != change_new {
-            //User has changed brightness
-            change = change_new;
-            if brightness == brightness_dev.get_brightness() {
-                brightness = brightness_dev.get_brightness();
-            } else {
-                brightness_dev.set_brightness(change);
+        if brightnessctl_status.load(Ordering::Relaxed) {
+            let change_new = change_calc(get_limit() as u8);
+            if change != change_new {
+                //User has changed brightness
+                change = change_new;
+                if brightness == brightness_dev.get_brightness() {
+                    brightness = brightness_dev.get_brightness();
+                } else {
+                    brightness_dev.set_brightness(change);
+                }
             }
+        } else if change != 0 {
+            brightness_dev.set_brightness(-change);
+            change = 0;
         }
         sleep(Duration::from_secs(get_refresh()));
     }
---
+++ src/sys_tray.rs
@@ -0,0 +1,102 @@
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+
+use ksni;
+
+struct SysTray {
+    running: Arc<AtomicBool>,
+}
+
+impl ksni::Tray for SysTray {
+    fn icon_name(&self) -> String {
+        if self.running.load(Ordering::Relaxed) {
+            "display-brightness-high-symbolic".into()
+        } else {
+            "display-brightness-off-symbolic".into()
+        }
+    }
+    fn activate(&mut self, _x: i32, _y: i32) {
+        if self.running.load(Ordering::Relaxed) {
+            self.running.store(false, Ordering::Relaxed)
+        } else {
+            self.running.store(true, Ordering::Relaxed)
+        }
+    }
+    fn title(&self) -> String {
+        "auto_backlight".into()
+    }
+    fn tool_tip(&self) -> ksni::ToolTip {
+        let description = if self.running.load(Ordering::Relaxed) {
+            "Auto backlight is running".into()
+        } else {
+            "Auto backlight is not running".into()
+        };
+        ksni::ToolTip {
+            title: "auto_backlight".into(),
+            description,
+            ..Default::default()
+        }
+    }
+    fn category(&self) -> ksni::Category {
+        ksni::Category::Hardware
+    }
+    fn menu(&self) -> Vec<ksni::MenuItem<Self>> {
+        use ksni::menu::*;
+        vec![
+            CheckmarkItem {
+                label: "Enabled".into(),
+                checked: self.running.load(Ordering::Relaxed),
+                activate: Box::new(|this: &mut Self| {
+                    if this.running.load(Ordering::Relaxed) {
+                        this.running.store(false, Ordering::Relaxed)
+                    } else {
+                        this.running.store(true, Ordering::Relaxed)
+                    }
+                }),
+                ..Default::default()
+            }
+            .into(),
+            StandardItem {
+                label: "Exit".into(),
+                icon_name: "application-exit".into(),
+                activate: Box::new(|_| std::process::exit(0)),
+                ..Default::default()
+            }
+            .into(),
+        ]
+    }
+}
+
+pub fn start_knsi(status: Arc<AtomicBool>) {
+    let service = ksni::TrayService::new(SysTray { running: status });
+    service.spawn();
+    loop {
+        std::thread::park();
+    }
+}
+#[cfg(test)]
+mod test {
+    use std::{
+        sync::{
+            atomic::{AtomicBool, Ordering},
+            Arc,
+        },
+        thread,
+        time::Duration,
+    };
+
+    use super::start_knsi;
+
+    #[test]
+    fn does_it_work() {
+        let new = Arc::new(AtomicBool::new(true));
+        let new2 = new.clone();
+        let _handle1 = thread::spawn(move || start_knsi(new));
+        loop {
+            println!("{}", new2.load(Ordering::Relaxed));
+            thread::sleep(Duration::from_secs(2));
+        }
+    }
+}
