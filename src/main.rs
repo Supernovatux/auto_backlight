49bdedaf5a9828a2e6bd39546f97072263f47f11
supernovatux <thulashitharan.d@gmail.com>
2022-09-12 18:01:18 +0530

    Added Lockfiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::{File, self};
+
 fn main() {
+    if File::open("/tmp/auto-backlight.lock").is_ok() {
+        eprintln!("Is another instance of the process running?");
+        eprintln!("If not then try deleting /tmp/auto-backlight.lock");
+        return;
+    }
+    if File::create("/tmp/auto-backlight.lock").is_err() {
+        eprintln!("Failed to create lock file.");
+        eprintln!("Exitting");
+        return;
+    }
     futures::executor::block_on(auto_backlight::init());
+    if fs::remove_file("/tmp/auto-backlight.lock").is_err() {
+        eprintln!("Failed to remove the lockfile");
+    }
 }
